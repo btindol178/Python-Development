3f67ddd4c88485320d6168c45d9cac141340ff9b
btindol178 <datamaster178@gmail.com>
2020-12-06 16:53:14 -0500

    Add files via upload

---
+++ VirtualEnvironmentOne.py
@@ -0,0 +1,16 @@
+# CREATE VIRTUAL ENVIRONMENT !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+###############################################################################################################################################
+# make a virtual enviornment with (conda create --name myDjangoEnv django)  the enviornment is DjangoMyEnv and the  theing loaded is django
+# say no to load thing
+# conda create --name MyDjangoEnv python=3.5
+# then say yes with y
+
+# List Envirnments
+#conda info --envs
+
+# ACTIVATE ENVIRONMENT
+#activate MyDjangoEnv
+
+#Deactivate when done
+#deactivate MyDjangoEnv
+###############################################################################################################################################
